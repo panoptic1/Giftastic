fd28d666a2a42950235f3306ed91e6ade4dc439c
Ryan Helgerson <ryan.helgerson@gmail.com>
2018-04-19 18:28:10 -0500

    Style elements.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -7,8 +7,10 @@ var celebrities = ["Nicolas Cage", "Anna Ferris", "Bruce Campbell", "Kathy Griff
 //Function for grabbing info from the API when the button gets pushed
 function displayCeleb () {
     var celeb = $(this).attr("data-name");
+    console.log(celeb);
+    console.log($(this));
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + celeb +"&api_key=cYspV105zCuhSY2GlSUP66RGIjGSLURD&limit=10";
-
+    console.log(queryURL);
     //Create an ajax call for the celebrity that is clicked
     $.ajax({
         url: queryURL,
@@ -20,25 +22,34 @@ function displayCeleb () {
             console.log(i);
             //Create a variable for a div that will contain the rating and the picture, which will nest inside "#celebrities-view"
             var celebDiv = $("<div class=\"search-result\">")
-            console.log("celebDiv",celebDiv);
+            
+            //Create variables for data associated with the rating, the still image, and the animated gif. 
             var rating = response.data[i].rating;
             var still = response.data[i].images.fixed_height_still.url;
             var animated = response.data[i].images.fixed_height.url; 
-            console.log("still",still);
-            console.log("animated", animated);
-            var celebImage = $("<img>")
+            
+            //Create a variable to dynamically insert an image element to the DOM and a p element for the rating
+            var celebImage = $("<img>");
+            var p = $("<p>").text("rating:" + rating);
+
+            //Use data-* method to add data attributes for the still and animated versions of the gif
             celebImage.attr("src", still);
             celebImage.attr("data-still", still);
             celebImage.attr("data-animate", animated);
             celebImage.attr("data-state", "still"); //Ask about this
+
+            //Add a class to the celebImage variable to target it for a click event in another function
             celebImage.addClass("celeb-image");
-            var p = $("<p>").text("rating:" + rating)
+            
+            //Append the p variable and the celebImage variable to the celebDiv variable
             celebDiv.append(p);
             celebDiv.append(celebImage);
-            console.log("celebDiv", celebDiv);
+
+            //Append the celebDiv variable to the #celebrities-view div element in the DOM
             $("#celebrities-view").append(celebDiv);
         }
         
+        //Create an event listener which will toggle the animated link when the user clicks on the gif. 
         $(document).on("click", ".celeb-image", function(){
             var state = $(this).attr("data-state");
             if (state === "still"){
@@ -55,7 +66,7 @@ function displayCeleb () {
     });
 }
 
-$("#buttons").on("click", displayCeleb);
+$(document).on("click", ".celebrity", displayCeleb);
 
 //Function to generate buttons
 function renderButtons() {
@@ -69,8 +80,10 @@ function renderButtons() {
 
       // Then dynamicaly generate buttons for each movie in the array.
       var button = $("<button>");
-      // Add a class
+      // Add a class to use for the on-click event
       button.addClass("celebrity");
+      //Add a bootstrap class to create some space
+      button.addClass("mr-3");
       // Add a data attribute with a value of the movie at index i
       button.attr("data-name", celebrities[i]);
       // Provide the button's text with a value of the movie at index i
@@ -97,5 +110,4 @@ function renderButtons() {
   //Call renderbuttons upon page load so that the initial array displays
   renderButtons();
 
-//Create a function that dumps search results from the celeb buttons into the div
-  //Make sure it dumps the first ten results, as well as the rating for each.
+
